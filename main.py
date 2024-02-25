46ac6a85f1d171a659679c97ed554fa2af125162
Zeyad Shaban <zeyadShapan2004@gmail.com>
2024-02-25 10:26:47 +0200

    focal lenght set to 2.94 for go pro hero 4 black

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     connectionString = "udpin:172.26.240.1:14550"
 
     sonya6000 = Camera(spacingAt100Alt=47, focalLength=20, imgWidth=6000, imgHeight=4000, sensorWidth=23.5, sensorHeight=15.6)
-    # ! in the code focal length was 2.94, in mission planner it is 2.5
-    goProHero4Black = Camera(spacingAt100Alt=100.33, focalLength=2.5, imgWidth=40000, imgHeight=3000, sensorWidth=6.17, sensorHeight=4.56)
+    # ! old code focal length was 2.94, in mission planner it is 2.5
+    goProHero4Black = Camera(spacingAt100Alt=100.33, focalLength=2.94, imgWidth=40000, imgHeight=3000, sensorWidth=6.17, sensorHeight=4.56)
 
     wpPath = './data/Waypoints.csv'
     obsPath = './data/Obstacles.csv'
