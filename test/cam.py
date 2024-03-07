f3da7619e407064885da691bdfe85f1ed66d632d
Zeyad Shaban <zeyadShapan2004@gmail.com>
2024-03-07 09:11:14 +0200

    code for sender and receiver video stream

--- test/cam.py
+++ test/cam.py
@@ -1,12 +1,6 @@
 import cv2
-import numpy as np
-import time
 
-cap = cv2.VideoCapture(1)
-
-# AI TEAM STOP BEING LAZY
-def shouldCapture(frame) -> bool:
-    return False
+cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
@@ -14,10 +8,6 @@ while True:
     if not ret:
         print("frame not read correctly")
         break
-
-    if shouldCapture(frame):
-        ret, frame = cap.read()
-        cv2.imwrite('black_frame.png', frame)
 
     cv2.imshow("Camera", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
